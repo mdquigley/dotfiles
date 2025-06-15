8836d43dcadb9a7a3bc8d9c063efcc06a9f6f8fd
Mike Quigley <mdq204@gmail.com>
2025-06-15 14:29:03 -0400

    Clean up make script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -14,20 +14,18 @@ files="zshrc oh-my-zsh alias private"    # list of files/folders to symlink in h
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+echo "Creating $olddir for backup of any existing dotfiles in ~/"
 mkdir -p $olddir
-echo "done"
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
+echo "Changing to the $dir directory"
 cd $dir
-echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
+    mv ~/.$file $olddir/
+    echo "Creating symlink to $file in ~/"
     ln -s $dir/$file ~/.$file
 done
 
